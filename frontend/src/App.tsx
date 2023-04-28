4aeb7fd364e54ff422784f96c851893886cc6599
Ewelina <ewelinazawol@gmail.com>
2023-04-28 17:06:13 +0100

    Init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Leafy Lane</h1>
+      </header>
+      <main></main>
+      <footer>Leafy Lane 2023</footer>
+    </div>
+  );
+}
+
+export default App;
