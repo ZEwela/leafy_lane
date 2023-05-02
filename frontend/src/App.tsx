4c9c26991dd8e58d776a30ad8f2c7a78908603d4
Ewelina <ewelinazawol@gmail.com>
2023-05-02 13:37:26 +0100

    Routing with react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
-import { sampleProducts } from "./data";
-import { Container, Navbar, Nav, Row, Col } from "react-bootstrap";
-
+import { Container, Navbar, Nav } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
 function App() {
   return (
     <div className="d-flex flex-column vh-100">
@@ -25,19 +24,7 @@ function App() {
       </header>
       <main>
         <Container className="mt-5">
-          <Row>
-            {sampleProducts.map((product) => (
-              <Col key={product.slug} sm={6} md={4} lg={3}>
-                <img
-                  src={product.image}
-                  alt={product.name}
-                  className="product-image image-responsive"
-                />
-                <h2>{product.name}</h2>
-                <p>£ {product.price}</p>
-              </Col>
-            ))}
-          </Row>
+          <Outlet />
         </Container>
       </main>
       <footer>
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,25 @@
+import { Col, Row } from "react-bootstrap";
+import { sampleProducts } from "../data";
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <Row>
+      {sampleProducts.map((product) => (
+        <Col key={product.slug} sm={6} md={4} lg={3}>
+          <Link to={`/product/${product.slug}`}>
+            <img
+              src={product.image}
+              alt={product.name}
+              className="product-image image-responsive"
+            />
+            <h2>{product.name}</h2>
+            <p>£ {product.price}</p>
+          </Link>
+        </Col>
+      ))}
+    </Row>
+  );
+}
+
+export default Home;
